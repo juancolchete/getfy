a91b881cfb8a826a537893b3f48070e320d8ab19
juancolchete <juanudkp@gmail.com>
2024-06-26 15:05:08 +0000

    feat: send get request with parameters

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
+import axios from "axios"
+
 const getify = async (url:string)=>{
   const urlAndParamsRaw = url.split("?")
   const urlBase = urlAndParamsRaw[0]
   const paramsRaw =  urlAndParamsRaw[1].split("&")
-  const params = {}
+  let urlVars = {}
   for(let i =0;i < paramsRaw.length; i++){
     const keyValue = paramsRaw[i].split("=") 
-    params[keyValue[0]] = keyValue[1]
+    if(keyValue[0] == "params"){
+      urlVars[keyValue[0]] = JSON.parse(keyValue[1])
+    }else{
+      urlVars[keyValue[0]] = keyValue[1]
+    }
   }
-  console.log(urlBase,params)
+  let reqUrl = `${urlBase}?`;
+  const paramsKeys = Object.keys(urlVars["params"])
+  const paramsValues = Object.values(urlVars["params"])
+  console.log(paramsKeys)
+  for(let i=0;i<paramsKeys.length;i++){
+    if(i == paramsKeys.length -1){
+      reqUrl += `${paramsKeys[i]}=${paramsValues[i]}`  
+    }else{
+      reqUrl += `${paramsKeys[i]}=${paramsValues[i]}&`
+    }
+  }
+  axios.get(reqUrl)
   return "getify"
 }
 
