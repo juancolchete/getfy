754e4839cb97a8679c7aba3193f777fb887912e5
juancolchete <juanudkp@gmail.com>
2024-06-26 14:12:13 +0000

    feat: get url and parameters

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-const getify = async ()=>{
+const getify = async (url:string)=>{
+  const urlAndParamsRaw = url.split("?")
+  const urlBase = urlAndParamsRaw[0]
+  const paramsRaw =  urlAndParamsRaw[1].split("&")
+  const params = {}
+  for(let i =0;i < paramsRaw.length; i++){
+    const keyValue = paramsRaw[i].split("=") 
+    params[keyValue[0]] = keyValue[1]
+  }
+  console.log(urlBase,params)
   return "getify"
 }
-
 
 export default getify
